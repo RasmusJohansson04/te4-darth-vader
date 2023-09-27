e5ae4b76ec06ec8023a2b277878c4db2e49d6c53
= <=>
2023-09-27 12:41:56 +0200

    Funkar kanske

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,52 @@ import Spinner from './components/Spinner'
 import './App.css'
 
 function App() {
-  const [lat, setLat] = useState([])
-  const [long, setLong] = useState([])
+  const [lat, setLat] = useState(() => {
+    return JSON.parse(localStorage.getItem('lat')) || []
+  })
+  const [long, setLong] = useState(() => {
+    return JSON.parse(localStorage.getItem('long')) || []
+  })
   const [weatherData, setWeatherData] = useState([])
 
+  const [weatherDataSaved, setWeatherDataSaved] = useState(() => {
+    return JSON.parse(localStorage.getItem('weatherDataSaved')) || []
+  })
+  const [dataAge, setDataAge] = useState(() => {
+    return JSON.parse(localStorage.getItem('dataAge')) || []
+  })
+
+  // 1. Get location from browser
+  // 2. Check if location is the same as last time
+  // 3. If location is the same as last time, use data from localstorage
+  // 4. IF data is older than 10 minutes, fetch new data
+  // 5. If location is not the same as last time, fetch new data
+  // 6. Save data to localstorage
   useEffect(() => {
     console.log(`Before call: ${typeof (weatherData)}`)
+
     async function fetchWeather() {
       navigator.geolocation.getCurrentPosition((position) => {
-        setLat(position.coords.latitude)
-        setLong(position.coords.longitude)
+        if (lat === position.coords.latitude && long === position.coords.longitude) {
+          if (new Date().getTime() - dataAge < 600000) {
+            setWeatherData(weatherDataSaved)
+            console.log('Not old')
+            return
+          }
+        }
       })
-      console.log(lat, long)
 
-      if (lat.length === 0 || long.length === 0) {
-        return
-      }
-
+      console.log('Fetching')
       const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?'
       await fetch(`${baseUrl}lat=${lat}&lon=${long}&appid=${import.meta.env.VITE_API_KEY}`)
         .then(res => res.json())
         .then(result => {
-          console.log(result)
           setWeatherData(result)
+          localStorage.setItem('lat', JSON.stringify(lat))
+          localStorage.setItem('long', JSON.stringify(long))
+          localStorage.setItem('weatherDataSaved', JSON.stringify(result))
+          localStorage.setItem('dataAge', JSON.stringify(new Date().getTime()))
+          console.log(result)
         }).catch(err => {
           console.log(err)
         })
